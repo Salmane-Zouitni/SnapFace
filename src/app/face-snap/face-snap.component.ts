ef28eb11a491903b703949725890f2a67e2edf2b
Salmane-Zouitni <salmane.zouitni@gmail.com>
2023-01-14 23:29:14 +0100

    removing unwanted template

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+
+}
