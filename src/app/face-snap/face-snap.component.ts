23e9da6b13c2e0ad6761d1d64f5e12710fa64f67
Salmane-Zouitni <salmane.zouitni@gmail.com>
2023-01-15 00:18:16 +0100

    adding button to increment snaps

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
-  styleUrls: ['./face-snap.component.scss']
+  styleUrls: ['./face-snap.component.scss'],
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit() {
+    this.title = 'Sonixe';
+    this.description =
+      'Sonixe is a social media platform for sharing photos and videos.';
+    this.createdDate = new Date();
+    this.snaps = 7;
+    this.imageUrl =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+  onAddSnap() {
+    this.snaps++;
+  }
 }
