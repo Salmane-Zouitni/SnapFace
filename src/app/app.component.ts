b5fa2ec63197c732a85b71818a7f963027852db4
Salmane-Zouitni <salmane.zouitni@gmail.com>
2023-01-17 00:40:33 +0100

    refactoring the code so the components generation became dynamique with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,38 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  mySecondSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Salmane',
-      description:
-        'Salmane is a social media platform for sharing photos and videos.',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'paris',
-    };
-    this.mySecondSnap = {
-      title: 'alikhandroo',
-      description:
-        'Manrtiniss is a social media platform for sharing photos and videos.',
-      imageUrl: 'https://i.ibb.co/VBvdyw9/maxresdefault.jpg',
-      createdDate: new Date(),
-      snaps: 1,
-      location: 'Mexico',
-    };
-    this.myLastSnap = {
-      title: 'Gujo Saturo',
-      description:
-        'Gujo Saturo is one of the most famous Japanese artists. He is known for his',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 2,
-    };
+    this.faceSnaps = [
+      {
+        title: 'Salmane',
+        description:
+          'Salmane is a social media platform for sharing photos and videos.',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'paris',
+      },
+      {
+        title: 'alikhandroo',
+        description:
+          'Manrtiniss is a social media platform for sharing photos and videos.',
+        imageUrl: 'https://i.ibb.co/VBvdyw9/maxresdefault.jpg',
+        createdDate: new Date(),
+        snaps: 1,
+        location: 'Mexico',
+      },
+      {
+        title: 'Gujo Saturo',
+        description:
+          'Gujo Saturo is one of the most famous Japanese artists. He is known for his',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 2,
+      },
+    ];
   }
 }
